ad59e7238661dc9419d79d64185d3b052a1fb573
mvezer <vezer.m@gmail.com>
2017-02-11 22:14:48 +0100

    input parsing fixed

--- knapsack.js
+++ knapsack.js
@@ -1,26 +1,26 @@
 function processData(input) {
-    var inputArr = String(input).split('\n');
-    var t = parseInt(inputArr[0]);
-    for (var i = 1; i < t; i+=2) {
-    	var n = parseInt(inputArr[i].split(' ')[0]);
-    	var k = parseInt(inputArr[i].split(' ')[1]);
+	var inputArr = String(input).split('\n');
+	var t = parseInt(inputArr[0]);
+	for (var i = 0; i < t; i++) {
+		var n = parseInt(inputArr[i * 2 + 1].split(' ')[0]);
+		var k = parseInt(inputArr[i * 2 + 1].split(' ')[1]);
 
-    	var arr = inputArr[i + 1].split(' ');
-    	arr = arr.map(Number);
+		var arr = inputArr[i * 2 + 2].split(' ');
+		arr = arr.map(Number);
 
-    	
-    }
-} 
+		//console.log(n, k, arr);
+	}
+}
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 _input = "";
-process.stdin.on("data", function (input) {
-    _input += input;
+process.stdin.on("data", function(input) {
+	_input += input;
 });
 
-process.stdin.on("end", function () {
-   processData(_input);
+process.stdin.on("end", function() {
+	processData(_input);
 });
 
 
